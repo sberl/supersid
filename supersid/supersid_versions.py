832a2f8a6fc2d25b8237e5d391fbf4000464a86c
sberl <steveberl@gmail.com>
2021-12-29 15:34:34 -0800

    Quick fix for broken dependencies for pandas. Add pandas to supersid_versions.py Issue #40

--- supersid/supersid_versions.py
+++ supersid/supersid_versions.py
@@ -36,10 +36,13 @@ tested_versions = {
     'cycler': {'versions': ['0.10.0', '0.11.0'], 'requirement': 'mandatory'},
     'six': {'versions': ['1.12.0', '1.16.0'], 'requirement': 'mandatory'},
     'kiwisolver': {'versions': ['1.3.1', '1.3.2'], 'requirement': 'mandatory'},
-    'numpy': {'versions': ['1.21.2', '1.21.3', '1.21.4+vanilla'], 'requirement': 'mandatory'},
+    'numpy': {'versions': ['1.21.2', '1.21.3', '1.21.4+vanilla', '1.21.5'], 'requirement': 'mandatory'},
     'pillow': {'versions': ['8.4.0'], 'requirement': 'mandatory'},
     'pyparsing': {'versions': ['2.4.7', '3.0.4'], 'requirement': 'mandatory'},
     'python-dateutil': {'versions': ['2.8.2'], 'requirement': 'mandatory'},
+
+    # pandas dependencies
+    'pandas': {'versions': ['1.3.5'], 'requirement': 'mandatory'},
 
     # sounddevice dependencies
     'sounddevice': {'versions': ['0.4.3'], 'requirement': 'optional'},
@@ -59,7 +62,8 @@ tested_versions = {
     'pip': {'versions': ['21.0.1', '21.2.2', '21.3.1'], 'requirement': 'mandatory'},
     'pyaudio': {'versions': ['0.2.11'], 'requirement': 'optional'},
     'pyalsaaudio': {'versions': ['0.9.0'], 'requirement': 'optional'},
-    'setuptools': {'versions': ['40.8.0', '57.4.0', '58.0.4'], 'requirement': 'mandatory'},    # import pkg_resources
+    # import pkg_resources
+    'setuptools': {'versions': ['40.8.0', '57.4.0', '58.0.4'], 'requirement': 'mandatory'},
 
     # builtin
     'itertools': {'versions': ['builtin'], 'requirement': 'mandatory'},
@@ -93,18 +97,24 @@ if __name__ == '__main__':
     for module in tested_versions:
         try:
             if module == 'python':
-                identified_versions[module] = "{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
+                identified_versions[module] = "{}.{}.{}".format(
+                    sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
             elif (module in sys.builtin_module_names):
                 identified_versions[module] = 'builtin'
             else:
-                identified_versions[module] = pkg_resources.get_distribution(module).version
+                identified_versions[module] = pkg_resources.get_distribution(
+                    module).version
 
             if identified_versions[module] in tested_versions[module]['versions']:
-                print("SUCCESS: {} '{}' found version {}".format(tested_versions[module]['requirement'], module, identified_versions[module]))
+                print("SUCCESS: {} '{}' found version {}".format(
+                    tested_versions[module]['requirement'], module, identified_versions[module]))
             else:
-                print("WARNING: {} '{}' found version {}, expected version(s) {}".format(tested_versions[module]['requirement'], module, identified_versions[module], tested_versions[module]['versions']))
+                print("WARNING: {} '{}' found version {}, expected version(s) {}".format(
+                    tested_versions[module]['requirement'], module, identified_versions[module], tested_versions[module]['versions']))
         except Exception as e:
             if 'mandatory' == tested_versions[module]['requirement']:
-                print("ERROR: {} '{}' not found".format(tested_versions[module]['requirement'], module))
+                print("ERROR: {} '{}' not found".format(
+                    tested_versions[module]['requirement'], module))
             else:
-                print("INFO: {} '{}' not found".format(tested_versions[module]['requirement'], module))
+                print("INFO: {} '{}' not found".format(
+                    tested_versions[module]['requirement'], module))
