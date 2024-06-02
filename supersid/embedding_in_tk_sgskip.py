b9b2a4fb0d6e87e9e5019d5a770266feb36fa554
fenrog <klaus@khoffmann.de>
2024-06-02 14:53:28 +0200

    converted TK GUI into a class

--- supersid/embedding_in_tk_sgskip.py
+++ supersid/embedding_in_tk_sgskip.py
@@ -24,59 +24,71 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 
-tk_root = tk.Tk()
-tk_root.wm_title("Embedding in Tk")
+class tkSidViewer():
+    """Create the Tkinter GUI."""
 
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-ax = fig.add_subplot()
-line, = ax.plot(t, 2 * np.sin(2 * np.pi * t))
-ax.set_xlabel("time [s]")
-ax.set_ylabel("f(t)")
+    def __init__(self):
+        self.tk_root = tk.Tk()
+        self.tk_root.wm_title("Embedding in Tk")
 
-canvas = FigureCanvasTkAgg(fig, master=tk_root)  # A tk.DrawingArea.
-canvas.draw()
+        fig = Figure(figsize=(5, 4), dpi=100)
+        self.t = np.arange(0, 3, .01)
+        ax = fig.add_subplot()
+        self.line, = ax.plot(self.t, 2 * np.sin(2 * np.pi * self.t))
+        ax.set_xlabel("time [s]")
+        ax.set_ylabel("f(t)")
 
-# pack_toolbar=False will make it easier to use a layout manager later on.
-toolbar = NavigationToolbar2Tk(canvas, tk_root, pack_toolbar=False)
-toolbar.update()
+        self.canvas = FigureCanvasTkAgg(fig, master=self.tk_root)  # A tk.DrawingArea.
+        self.canvas.draw()
 
-canvas.mpl_connect(
-    "key_press_event", lambda event: print(f"you pressed {event.key}"))
-canvas.mpl_connect("key_press_event", key_press_handler)
+        # pack_toolbar=False will make it easier to use a layout manager later on.
+        toolbar = NavigationToolbar2Tk(self.canvas, self.tk_root, pack_toolbar=False)
+        toolbar.update()
 
-button_quit = tk.Button(master=tk_root, text="Quit", command=tk_root.destroy)
+        self.canvas.mpl_connect(
+            "key_press_event", lambda event: print(f"you pressed {event.key}"))
+        self.canvas.mpl_connect("key_press_event", key_press_handler)
+
+        button_quit = tk.Button(master=self.tk_root, text="Quit", command=self.tk_root.destroy)
+
+        slider_update = tk.Scale(self.tk_root, from_=1, to=5, orient=tk.HORIZONTAL,
+                                      command=self.update_frequency, label="Frequency [Hz]")
+
+        # Packing order is important. Widgets are processed sequentially and if there
+        # is no space left, because the window is too small, they are not displayed.
+        # The canvas is rather flexible in its size, so we pack it last which makes
+        # sure the UI controls are displayed as long as possible.
+        button_quit.pack(side=tk.BOTTOM)
+        slider_update.pack(side=tk.BOTTOM)
+        toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def update_frequency(self, new_val):
+        # retrieve frequency
+        f = float(new_val)
+
+        # update data
+        y = 2 * np.sin(2 * np.pi * f * self.t)
+        self.line.set_data(self.t, y)
+
+        # required to update canvas and attached toolbar!
+        self.canvas.draw()
+
+        if gc.garbage:
+            print("gc.garbage")     # did not yet trigger
+            print(gc.garbage)       # did not yet trigger
+
+        objgraph.show_growth()      # triggers rarely when klicking the cntrol for
+                                    # the frequency and moving the mouse wildly
+
+    def run(self):
+        self.tk_root.mainloop()
 
 
-def update_frequency(new_val):
-    # retrieve frequency
-    f = float(new_val)
-
-    # update data
-    y = 2 * np.sin(2 * np.pi * f * t)
-    line.set_data(t, y)
-
-    # required to update canvas and attached toolbar!
-    canvas.draw()
-
-    if gc.garbage:
-        print("gc.garbage")     # did not yet trigger
-        print(gc.garbage)       # did not yet trigger
-
-    objgraph.show_growth()      # triggers rarely when klicking the cntrol for
-                                # the frequency and moving the mouse wildly
+def main():
+    viewer = tkSidViewer()
+    viewer.run()
 
 
-slider_update = tk.Scale(tk_root, from_=1, to=5, orient=tk.HORIZONTAL,
-                              command=update_frequency, label="Frequency [Hz]")
-
-# Packing order is important. Widgets are processed sequentially and if there
-# is no space left, because the window is too small, they are not displayed.
-# The canvas is rather flexible in its size, so we pack it last which makes
-# sure the UI controls are displayed as long as possible.
-button_quit.pack(side=tk.BOTTOM)
-slider_update.pack(side=tk.BOTTOM)
-toolbar.pack(side=tk.BOTTOM, fill=tk.X)
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-tk.mainloop()
+if __name__ == "__main__":
+    main()
