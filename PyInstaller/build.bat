d19349dbde4df017bc9cd6c53ca7179d9d34cbfa
fenrog <klaus@khoffmann.de>
2025-06-15 19:52:01 +0200

    PyInstaller command line change after switch to Python 3.13.5

--- PyInstaller/build.bat
+++ PyInstaller/build.bat
@@ -1,15 +1,22 @@
 REM preconditions:
 REM pip install --upgrade pyinstaller
 
+REM rmdir /S /Q build
+REM rmdir /S /Q dist
+REM rmdir /S /Q spec
 rmdir /S /Q ..\Program
 mkdir ..\Program
 del ..\SuperSID.zip
+
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\config.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\ftp_to_stanford.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\sampler.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\sidfile.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\supersid.py
+python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm --copy-metadata=readchar ..\supersid\supersid.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\supersid_plot.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\supersid_plot_gui.py
 python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm ..\supersid\supersid_scanner.py
+python -m PyInstaller --icon=..\supersid.ico --specpath specs --noconfirm --copy-metadata=readchar ..\supersid\supersid_scanner.py
+
 python copy_dist.py
