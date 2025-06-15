e71c88bf5ea0c062b101ffed8db5b180a5a4536e
fenrog <klaus@khoffmann.de>
2025-06-15 10:06:08 +0200

    checking for realtime violation in SidTimer._ontimer()

--- supersid/sidtimer.py
+++ supersid/sidtimer.py
@@ -16,14 +16,14 @@ import threading
 class SidTimer:
     """Keep track of time."""
 
-    def __init__(self, interval, callback, delay=0):
+    def __init__(self, interval, callback):
         """Synchronize the timer and start the trigger mechanism.
 
         Public properties:
         - start_time: reference starting time.time() in local time
             *on the interval* (synchro)
         - expected_time: theoretical time the trigger should happen
-            as 'start_time + X * interval'
+            as 'start_time synchronized to a multiple of interval'
         - time_now: real time.time() when the trigger happened
         """
         self.version = "1.3.1 20130907"
@@ -42,7 +42,7 @@ class SidTimer:
             now = time.gmtime()
         #  request a Timer
         self.start_time = int(time.time() / self.interval) \
-            * self.interval + delay
+            * self.interval
         self.expected_time = self.start_time + self.interval
         self._timer = threading.Timer(self.expected_time - time.time(),
                                       self._ontimer)
@@ -53,6 +53,11 @@ class SidTimer:
 
         with adjustment if necessary
         i.e. running late/fast then perform callback
+
+        If _ontimer() triggers before expected_time or after expected_time_max,
+        this is an error that can cause the hourly save to be missed. Even worse
+        the creation of a new day could be missed. Let's create a warning on the
+        console if an issue with the timer reliabilitry is identified.
         """
         with self.lock:     # only one timer callback at a time
             self.time_now = time.time()
@@ -65,6 +70,14 @@ class SidTimer:
                                    * 3600
                                    + self.utc_now.minute
                                    * 60 + self.utc_now.second) / self.interval)
+
+            expected_time_max = self.expected_time + self.interval
+            if (self.time_now < self.expected_time) or (self.time_now >= expected_time_max):
+                print("WARNING: Hard realtime violation in SidTimer._ontimer(). "
+                      f"expected: [{self.expected_time}..{expected_time_max}) "
+                      f"found: {self.time_now}. "
+                      "Please report at 'https://github.com/sberl/supersid/issues/107'. ")
+
             self.expected_time += self.interval
             # callback to perform tasks
             self.callback()
@@ -80,7 +93,8 @@ class SidTimer:
 
 if __name__ == '__main__':
     TIME_INTERVAL = 5.0  # seconds
-    TEST_LENGTH = 3600.0  # seconds
+    TEST_LENGTH = 90.0  # seconds
+    FORCE_ERROR = True
 
     class TestSidTimerSubclass(SidTimer):
         """Example of SidTimer implementation.
@@ -89,7 +103,7 @@ if __name__ == '__main__':
         """
 
         def __init__(self, interval):
-            print("Waiting for synchro...", end='')
+            print("Waiting for synchronization ... ", end='')
             SidTimer.__init__(self, interval, self.on_timer_event)
             print("done.")
             self.max_plus_error, self.max_minus_error = 0, 0
@@ -100,7 +114,7 @@ if __name__ == '__main__':
             In this test class, only display some tracking on the
             timer's accuracy.
             """
-            time_error = self.expected_time - self.time_now - self.interval
+            time_error = self.time_now - (self.expected_time - self.interval)
             if time_error > 0 and time_error > self.max_plus_error:
                 self.max_plus_error = time_error
             elif time_error < 0 and time_error < self.max_minus_error:
@@ -109,7 +123,15 @@ if __name__ == '__main__':
             print("Idx", self.data_index, "now:", self.time_now,
                   "expect_time:", self.expected_time - self.interval)
             print(" err:", time_error,
-                  "interval: %f" % (self.expected_time - self.time_now))
+                  f"interval: {self.expected_time - self.time_now}",
+                  datetime.now(timezone.utc))
+            if FORCE_ERROR and ((self.time_now % 60) >= (60 - TIME_INTERVAL)):
+                # will trigger at hh:mm:55
+                # and cause hh:mm:00 .. hh:mm:04.9999 to be missed
+                print(f"FORCE_ERROR: sleeping {2 * TIME_INTERVAL} seconds at "
+                      f"{self.time_now} ...")
+                time.sleep(2 * TIME_INTERVAL)
+                print(f"FORCE_ERROR: ... wakeup at {time.time()}")
 
         def cancel_timer(self):
             """ Cancel the timer. """
@@ -122,7 +144,7 @@ if __name__ == '__main__':
         """
 
         def __init__(self, interval):
-            print("Waiting for synchro...", end='')
+            print("Waiting for synchronization ... ", end='')
             self.sid_timer = SidTimer(interval, self.on_timer_event)
             print("done.")
             self.max_plus_error, self.max_minus_error = 0, 0
@@ -130,10 +152,11 @@ if __name__ == '__main__':
         def on_timer_event(self):
             """Call back function to do tasks when Timer is triggered.
 
-            In this test , only display some tracking on the timer's accuracy
+            In this test class, only display some tracking on the
+            timer's accuracy.
             """
-            time_error = self.sid_timer.expected_time \
-                - self.sid_timer.time_now - self.sid_timer.interval
+            time_error = self.sid_timer.time_now - \
+                (self.sid_timer.expected_time - self.sid_timer.interval)
             if time_error > 0 and time_error > self.max_plus_error:
                 self.max_plus_error = time_error
             elif time_error < 0 and time_error < self.max_minus_error:
@@ -143,16 +166,25 @@ if __name__ == '__main__':
                   self.sid_timer.time_now, "expect_time:",
                   self.sid_timer.expected_time - self.sid_timer.interval)
             print(" err:", time_error,
-                  "interval: %f" % (self.sid_timer.expected_time
-                                  - self.sid_timer.time_now))
+                  f"interval: {self.sid_timer.expected_time - self.sid_timer.time_now}",
+                  datetime.now(timezone.utc))
+            if FORCE_ERROR and ((self.sid_timer.time_now % 60) >= (60 - TIME_INTERVAL)):
+                # will trigger at hh:mm:55
+                # and cause hh:mm:00 .. hh:mm:04.9999 to be missed
+                print(f"FORCE_ERROR: sleeping {2 * TIME_INTERVAL} seconds at "
+                      f"{self.sid_timer.time_now} ...")
+                time.sleep(2 * TIME_INTERVAL)
+                print(f"FORCE_ERROR: ... wakeup at {time.time()}")
 
         def cancel_timer(self):
-            """Cancel the timer."""
+            """ Cancel the timer. """
             self.sid_timer.stop()
 
 # ------------------------------------------------------------------------
-    # choose either 'TestSidTimerSimple' or 'TestSidTimerSubclass'.
-    # Results will be the same.
+    # Test both 'TestSidTimerSimple' and 'TestSidTimerSubclass'.
+    # Results shall be the same.
+
+    print("\nTestSidTimerSimple")
     tst = TestSidTimerSimple(TIME_INTERVAL)
     try:
         time.sleep(TEST_LENGTH)  # do nothing while testing timer's accuracy
@@ -161,5 +193,17 @@ if __name__ == '__main__':
 
     # cleanup and show max errors
     tst.cancel_timer()
-    print("max positive error: ", tst.max_plus_error)
-    print("max negative error: ", tst.max_minus_error)
+    print(f"mximum too late by: {tst.max_plus_error} seconds")
+    print(f"maximum too early by: {tst.max_minus_error} seconds")
+
+    print("\nTestSidTimerSubclass")
+    tst = TestSidTimerSubclass(TIME_INTERVAL)
+    try:
+        time.sleep(TEST_LENGTH)  # do nothing while testing timer's accuracy
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+    # cleanup and show max errors
+    tst.cancel_timer()
+    print(f"mximum too late by: {tst.max_plus_error} seconds")
+    print(f"maximum too early by: {tst.max_minus_error} seconds")
